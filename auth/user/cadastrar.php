638c8edee2bc6cffbb3683701c9c8ae837d9136b
Ricardo Santos <richard7santos@hotmail.com>
2024-06-19 11:38:09 -0300

    Cadastrar usuário

--- auth/user/cadastrar.php
+++ auth/user/cadastrar.php
@@ -1,52 +1,15 @@
 <?php
 include dirname(__FILE__) . '/../../database/config.php';
 
-if (isset($_POST['primeiro-nome'], $_POST['email'], $_POST['ultimo-nome'], $_POST['pass'])) {
-    $foto;
-    if (isset($_FILES['foto']['name']) && $_FILES["foto"]["error"] == 0) {
+if (isset($_POST['nome_completo'], $_POST['email'], $_POST['senha'])) {
 
-        $nomeArquivo = $_FILES['foto']['name'];
-        $tipoArquivo = $_FILES['foto']['type'];
-        $nomeTemporário = $_FILES['foto']['tmp_name'];
-        $tamanho = $_FILES['foto']['size'];
-        $erros = array();
-        $limitSize = 1024 * 1024 * 5;
-        $extensoesPermitidas = ["jpg", "jpeg", "png"];
-        $typesPermitidas = ["image/jpg", "image/jpeg", "image/png"];
-        $extensaoArquivo = pathinfo($nomeArquivo, PATHINFO_EXTENSION);
-
-        if ($tamanho > $limitSize) {
-            $erros[] = "Sua foto execede o tamanho máximo.<br>";
-        }
-        if (!in_array($extensaoArquivo, $extensoesPermitidas)) {
-            $erros[] = "Tipo de arquivo não permitido.<br>";
-        }
-        if (!in_array($tipoArquivo, $typesPermitidas)) {
-            $erros[] = "Tipo de arquivo não permitido.<br>";
-        }
-        if (!empty($erros)) {
-            foreach ($erros as $erro) {
-                print 'alert($erro)';
-            }
-        } else {
-            $destino = dirname(__FILE__) . "/../../assets/images/profileImages/";
-            $novoNome = $destino . $_POST['primeiro-nome'] . '.' . $extensaoArquivo;
-            $foto = $_POST['primeiro-nome'] . '.' . $extensaoArquivo;
-            if (move_uploaded_file($nomeTemporário, $novoNome)) {
-            }
-        }
-    }
-
-    $primeiro_nome = $_POST['primeiro-nome'];
-    $ultimo_nome = $_POST['ultimo-nome'];
+    $nome_completo = $_POST['nome_completo'];
     $email = $_POST['email'];
-    $senha = password_hash($_POST['pass'], PASSWORD_DEFAULT);
-    $tipo = $_POST['tipo'];
-    $data_nascimento = $_POST['dt-nasc'];
-    $foto_url = $foto ? $foto : '';
+    $senha = password_hash($_POST['senha'], PASSWORD_DEFAULT);
+    $telefone = $_POST['telefone'];
 
-    $sql = "INSERT INTO users (primeiro_nome, ultimo_nome, email, senha, tipo, data_nascimento, foto)
-                        values('$primeiro_nome', '$ultimo_nome', '$email', '$senha', '$tipo', '$data_nascimento', '$foto_url')";
+    $sql = "INSERT INTO user (nome_completo, email, senha, telefone)
+                        values('$nome_completo', '$email', '$senha', '$telefone')";
     $conn->query($sql);
     echo "<script>location.href='/login';</script>";
     exit;
