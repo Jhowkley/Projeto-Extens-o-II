57d22b7f596c70d724d95bfb2abb1e69f1bbdc03
Ricardo Santos <richard7santos@hotmail.com>
2024-06-22 13:37:27 -0300

    teste home

--- pages/home.php
+++ pages/home.php
@@ -4,6 +4,4 @@ include dirname(__FILE__) . '/../database/config.php';
 $sql = "SELECT * FROM item";
 $itens = $conn->query($sql) or die($conn->error);
 
-echo 'Pagina 1';
-
 include "../includes/home.html";
