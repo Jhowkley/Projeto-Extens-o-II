ce693e3683a3ea9a063827cb8a296e39d271c3bd
Ricardo Santos <richard7santos@hotmail.com>
2024-06-22 15:36:42 -0300

    .

--- pages/home.php
+++ pages/home.php
@@ -4,4 +4,4 @@ include dirname(__FILE__) . '/../database/config.php';
 $sql = "SELECT * FROM item";
 $itens = $conn->query($sql) or die($conn->error);
 
-include __DIR__ . "../includes/home.html";
+include __DIR__ . "/../includes/home.html";
