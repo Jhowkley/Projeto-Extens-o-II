a941a2100a53469a42c90dbf9b650ee160e5a487
Ricardo Santos <richard7santos@hotmail.com>
2024-06-22 15:56:41 -0300

    teste variável

--- pages/home.php
+++ pages/home.php
@@ -4,4 +4,7 @@ include dirname(__FILE__) . '/../database/config.php';
 $sql = "SELECT * FROM item";
 $itens = $conn->query($sql) or die($conn->error);
 
+print_r($itens);
+exit;
+
 include __DIR__ . '/../includes/home.html';
