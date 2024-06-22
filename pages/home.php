92f34f98eafb3ac76363577024b7a066db8e07c7
Ricardo Santos <richard7santos@hotmail.com>
2024-06-22 16:07:13 -0300

    teste novo

--- pages/home.php
+++ pages/home.php
@@ -4,7 +4,4 @@ include dirname(__FILE__) . '/../database/config.php';
 $sql = "SELECT * FROM item";
 $itens = $conn->query($sql) or die($conn->error);
 
-print_r($itens);
-exit;
-
 include __DIR__ . '/../includes/home.html';
